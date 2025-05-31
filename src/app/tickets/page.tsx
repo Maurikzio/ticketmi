4b80cc831a117a74fe484caaac6502bcbe8fb64a
Mauricio <m.morocho1196@gmail.com>
2025-05-30 22:38:58 -0500

    Added Card component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,13 +1,21 @@
 import { initialTickets } from "@/data";
 import Link from "next/link";
 import { ticketPath } from "@/paths";
-import clsx from "clsx";
 import { Pen, Check, File } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import clsx from "clsx";
 
 const TICKET_ICONS = {
-  "OPEN": <File />,
-  "IN_PROGRESS": <Pen />,
-  "DONE": <Check />
+  "OPEN": <File className="h-4 w-4" />,
+  "IN_PROGRESS": <Pen className="h-4 w-4" />,
+  "DONE": <Check className="h-4 w-4" />
 }
 
 export default async function Ticketspage() {
@@ -19,14 +27,23 @@ export default async function Ticketspage() {
       </div>
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map(ticket => (
-          <div key={ticket.id} className="w-full max-w-[420px] p-4 border border-slate-100 rounded">
-            <div>{TICKET_ICONS[ticket.status]}</div>
-            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
-            <p className={clsx("text-sm text-slate-500 truncate", {
-              "line-through": ticket.status === "DONE"
-            })}>{ticket.content}</p>
-            <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
-          </div>
+          <Card key={ticket.id} className="w-full max-w-[420px]">
+            <CardHeader>
+              <CardTitle className="flex gap-x-2 items-center">
+                <div>{TICKET_ICONS[ticket.status]}</div>
+                <h3 className="text-2xl">{ticket.title}</h3>
+              </CardTitle>
+              <CardDescription>Information of your ticket below.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <span className={clsx("line-clamp-3", { "line-through": ticket.status === "DONE" })}>
+                {ticket.content}
+              </span>
+            </CardContent>
+            <CardFooter>
+              <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
