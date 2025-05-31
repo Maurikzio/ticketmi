8133609d459255e1904ee258fbaa318e28872f26
Mauricio <m.morocho1196@gmail.com>
2025-05-30 23:08:14 -0500

    Created Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+import { homePath, ticketsPath } from "@/paths";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button"
+import { SquareChartGantt } from "lucide-react";
+
+const Header = () => {
+  return (
+    <nav
+      className="
+        supports-backdrop-blur:bg-background/60
+        fixed left-0 right-0 top-0 z-20
+        border-b bg-background/95 backdrop-blur
+        w-full flex py-2.5 px-5 justify-between
+      "
+    >
+      <div>
+        {/* <Button asChild variant="outline">
+              <Link href={homePath}>Home</Link>
+            </Button> */}
+        <Link href={homePath} className={buttonVariants({ variant: "ghost" })}>
+          <SquareChartGantt />
+          <h1 className=" text-lg font-bold">TicketMi</h1>
+        </Link>
+      </div>
+      <div>
+        <Link href={ticketsPath} className={buttonVariants({ variant: "default" })}>Tickets</Link>
+      </div>
+    </nav>
+  )
+};
+
+export default Header;
