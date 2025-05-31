c0e79d6e583e6721372c4b40b3b194bda2d4a8d3
Mauricio <m.morocho1196@gmail.com>
2025-05-30 22:41:39 -0500

    Added Separator component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -11,6 +11,7 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import clsx from "clsx";
+import { Separator } from "@/components/ui/separator";
 
 const TICKET_ICONS = {
   "OPEN": <File className="h-4 w-4" />,
@@ -25,6 +26,7 @@ export default async function Ticketspage() {
         <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
         <p className="text-sm">All your tickets at one place</p>
       </div>
+      <Separator />
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map(ticket => (
           <Card key={ticket.id} className="w-full max-w-[420px]">
