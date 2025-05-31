f1bcd154488fb5e3380ad0d24923fc904abbb9ca
Mauricio <m.morocho1196@gmail.com>
2025-05-31 14:41:20 -0500

    Updated ticket item component

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -4,13 +4,13 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import clsx from "clsx";
-import { Pen, Check, File } from "lucide-react";
+import { Pen, Check, File, SquarePen } from "lucide-react";
 import { Ticket } from "../definitions";
+import { Button } from "@/components/ui/button";
 
 const TICKET_ICONS = {
   "OPEN": <File className="h-4 w-4" />,
@@ -24,23 +24,29 @@ interface TicketItemProps {
 
 const TicketItem = ({ ticket }: TicketItemProps) => {
   return (
-    <Card className="w-full max-w-[420px]">
-      <CardHeader>
-        <CardTitle className="flex gap-x-2 items-center">
-          <div>{TICKET_ICONS[ticket.status]}</div>
-          <h3 className="text-2xl">{ticket.title}</h3>
-        </CardTitle>
-        <CardDescription>Information of your ticket below.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <span className={clsx("line-clamp-3", { "line-through": ticket.status === "DONE" })}>
-          {ticket.content}
-        </span>
-      </CardContent>
-      <CardFooter>
-        <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
-      </CardFooter>
-    </Card>
+    <div className="w-full max-w-[420px] flex gap-x-1">
+      <Card className="w-full ">
+        <CardHeader>
+          <CardTitle className="flex gap-x-2 items-center">
+            <div>{TICKET_ICONS[ticket.status]}</div>
+            <h3 className="text-2xl">{ticket.title}</h3>
+          </CardTitle>
+          <CardDescription>Information of your ticket below.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <span className={clsx("line-clamp-3", { "line-through": ticket.status === "DONE" })}>
+            {ticket.content}
+          </span>
+        </CardContent>
+      </Card>
+      <div className="flex flex-col gap-y-1">
+        <Button variant="outline" asChild size="icon">
+          <Link href={ticketPath(ticket.id)}>
+            <SquarePen />
+          </Link>
+        </Button>
+      </div>
+    </div>
   )
 };
 
