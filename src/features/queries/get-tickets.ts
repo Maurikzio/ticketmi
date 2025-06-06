eac042c3ca624cc1113407d23087f3e88d0e9459
Mauricio <m.morocho1196@gmail.com>
2025-06-05 21:19:02 -0500

    Creating form to crate ticket

--- src/features/queries/get-tickets.ts
+++ src/features/queries/get-tickets.ts
@@ -2,5 +2,5 @@ import { prisma } from "@/lib/prisma";
 import { Ticket } from "@prisma/client";
 
 export const getTickets = async (): Promise<Ticket[]> => {
-  return await prisma.ticket.findMany();
+  return await prisma.ticket.findMany({ orderBy: [{ createdAt: 'desc' }] })
 }
---
+++ src/features/ticket/components/ticket-create-form.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import createTicket from "../actions/create-ticket";
+
+const TicketCreateForm = () => {
+  return (
+    <form action={createTicket} className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" id="title" name="title" placeholder="Title" />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Label htmlFor="content">Content</Label>
+        <Textarea placeholder="Type the ticket content here." id="content" name="content" />
+      </div>
+
+      <Button>Create</Button>
+    </form>
+  )
+}
+
+export default TicketCreateForm;
