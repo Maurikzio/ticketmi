03c3908c1b8854503ce3e908aa7acc82f6b52fc3
Mauricio <m.morocho1196@gmail.com>
2025-06-06 21:41:32 -0500

    Using useTransition

--- src/features/ticket/components/ticket-create-form.tsx
+++ src/features/ticket/components/ticket-create-form.tsx
@@ -1,12 +1,22 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import createTicket from "../actions/create-ticket";
+import { useTransition } from "react";
+import { LoaderCircle } from "lucide-react";
 
 const TicketCreateForm = () => {
+  const [isPending, startTransition] = useTransition();
+
+  const createTicketAction = (formData: FormData) => {
+    startTransition(async () => await createTicket(formData))
+  }
+
   return (
-    <form action={createTicket} className="flex flex-col gap-4">
+    <form action={createTicketAction} className="flex flex-col gap-4">
       <div className="flex flex-col gap-2">
         <Label htmlFor="title">Title</Label>
         <Input type="text" id="title" name="title" placeholder="Title" />
@@ -17,7 +27,14 @@ const TicketCreateForm = () => {
         <Textarea placeholder="Type the ticket content here." id="content" name="content" />
       </div>
 
-      <Button>Create</Button>
+      <Button disabled={isPending}>
+        {isPending ? (
+          <>
+            <LoaderCircle className="h-4 w-4 animate-spin mr-4" />
+            {"Creating"}
+          </>
+        ) : "Create"}
+      </Button>
     </form>
   )
 }
--- src/features/ticket/components/ticket-update-form.tsx
+++ src/features/ticket/components/ticket-update-form.tsx
@@ -1,18 +1,27 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Ticket } from "@prisma/client";
 import updateTicket from "../actions/update-ticket";
+import { useTransition } from "react";
+import { LoaderCircle } from "lucide-react";
 
 interface TicketUpdateFormProps {
   ticket: Ticket
 }
 
 const TicketUpdateForm = ({ ticket }: TicketUpdateFormProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const updateTicketAction = (formData: FormData) => {
+    startTransition(async () => await updateTicket(ticket.id, formData))
+  }
 
   return (
-    <form action={updateTicket.bind(null, ticket.id)} className="flex flex-col gap-4">
+    <form action={updateTicketAction} className="flex flex-col gap-4">
       <div className="flex flex-col gap-2">
         <Label htmlFor="title">Title</Label>
         <Input type="text" id="title" name="title" placeholder="Title" defaultValue={ticket.title} />
@@ -23,7 +32,14 @@ const TicketUpdateForm = ({ ticket }: TicketUpdateFormProps) => {
         <Textarea placeholder="Type the ticket content here." id="content" name="content" defaultValue={ticket.content} />
       </div>
 
-      <Button>Update</Button>
+      <Button disabled={isPending}>
+        {isPending ? (
+          <>
+            <LoaderCircle className="h-4 w-4 animate-spin mr-4" />
+            {"Updating"}
+          </>
+        ) : "Update"}
+      </Button>
     </form>
   )
 }
