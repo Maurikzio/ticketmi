8e9059d61ee4e983c4ae0614fccbb5b285832c8b
Mauricio <m.morocho1196@gmail.com>
2025-06-22 17:38:10 -0500

    Adding mt tickets vs all tickets

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,14 +1,17 @@
 import CardCompact from "@/components/card-compact";
 import Heading from "@/components/heading";
 import Spinner from "@/components/spinner";
+import { requireProfile } from "@/features/auth/utils/requireProfile";
 import TicketCreateForm from "@/features/ticket/components/ticket-create-form";
 import TicketList from "@/features/ticket/components/ticket-list";
 import { Suspense } from "react";
 
-export default function Ticketspage() {
+export default async function Ticketspage() {
+  const profileData = await requireProfile();
+
   return (
     <div className="font-[family-name:var(--font-geist-sans)] flex-1 flex flex-col gap-y-8">
-      <Heading title="Tickets" description="All your tickets at one place" />
+      <Heading title="My Tickets" description="All your tickets at one place" />
 
       <CardCompact
         className="w-full max-w-[420px] self-center"
@@ -18,7 +21,7 @@ export default function Ticketspage() {
       />
 
       <Suspense fallback={<Spinner />}>
-        <TicketList />
+        <TicketList profileId={profileData?.profile.id} />
       </Suspense>
     </div>
   );
