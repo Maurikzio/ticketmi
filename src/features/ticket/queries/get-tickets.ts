2db371bafb2f617cd11b32fdbfa5922f036b98d6
Mauricio <m.morocho1196@gmail.com>
2025-06-21 22:50:58 -0500

    Including Profile to tickets query

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -2,5 +2,8 @@ import { prisma } from "@/lib/prisma";
 import { Ticket } from "@prisma/client";
 
 export const getTickets = async (): Promise<Ticket[]> => {
-  return await prisma.ticket.findMany({ orderBy: [{ createdAt: 'desc' }] })
+  return await prisma.ticket.findMany({
+    orderBy: [{ createdAt: 'desc' }],
+    include: { profile: true }
+  })
 }
