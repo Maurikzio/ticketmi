070aae03d75479306c29e1ea3ac224cde5af10b6
Mauricio <m.morocho1196@gmail.com>
2025-06-21 23:09:08 -0500

    Fixed undefined profile in ticket page

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
-import { Ticket } from "@prisma/client";
 
-export const getTickets = async (): Promise<Ticket[]> => {
+// export const getTickets = async (): Promise<Ticket[]> => {
+export const getTickets = async () => {
   return await prisma.ticket.findMany({
     orderBy: [{ createdAt: 'desc' }],
     include: { profile: true }
