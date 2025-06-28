cdf1d17787665b9156784a33e2ca906f22d033ea
Mauricio <m.morocho1196@gmail.com>
2025-06-27 21:50:48 -0500

    Fixed inconsistency between search and pagination

--- src/features/ticket/components/ticket-pagination.tsx
+++ src/features/ticket/components/ticket-pagination.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import { useQueryStates } from "nuqs";
-import { paginationOptions, paginationParser } from "../definitions";
+import { useQueryState, useQueryStates } from "nuqs";
+import { paginationOptions, paginationParser, searchParser } from "../definitions";
 import Pagination from "@/components/pagination";
+import { useEffect, useRef } from "react";
 
 interface TicketPaginationProps {
   paginatedTicketMetadata: {
@@ -12,8 +13,18 @@ interface TicketPaginationProps {
 }
 
 const TicketPagination = ({ paginatedTicketMetadata }: TicketPaginationProps) => {
-
   const [pagination, setPagination] = useQueryStates(paginationParser, paginationOptions)
+  const [search] = useQueryState("search", searchParser);
+  const prevSearch = useRef(search); //save the most recent search
+
+  useEffect(() => {
+    if (search === prevSearch.current) return; //compare current search with most recent search
+    // if search changed
+    prevSearch.current = search;
+
+    setPagination({ ...pagination, page: 0 })
+  }, [pagination, search, setPagination])
+
   return (
     <Pagination
       pagination={pagination}
