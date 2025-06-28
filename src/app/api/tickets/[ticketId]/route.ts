e23937484265c9eacaaefa67c5c01f9189408dd0
Mauricio <m.morocho1196@gmail.com>
2025-06-28 10:46:03 -0500

    Fixed type for params in dyanmic segment route

--- src/app/api/tickets/[ticketId]/route.ts
+++ src/app/api/tickets/[ticketId]/route.ts
@@ -1,11 +1,12 @@
 import { getTicket } from "@/features/ticket/queries/get-ticket"
 
 type RouteParams = {
-  params: {
+  params: Promise<{
     ticketId: string;
-  }
+  }>
 }
 export async function GET(_request: Request, { params }: RouteParams) {
-  const ticket = await getTicket(params.ticketId)
+  const { ticketId } = await params;
+  const ticket = await getTicket(ticketId)
   return Response.json(ticket)
 }
