a065c41f508d3b0a0b46fcbe816df8855f9d6e1d
Mauricio <m.morocho1196@gmail.com>
2025-07-01 12:53:24 -0500

    Using React Query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import Sidebar from "@/components/sidebar/sidebar";
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import QueryProvider from "./_providers/query-provider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,23 +39,25 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <NuqsAdapter>
-            <Header />
-            <div className="flex h-screen overflow-hidden border-collapse">
-              <Sidebar />
-              <main
-                className="
+          <QueryProvider>
+            <NuqsAdapter>
+              <Header />
+              <div className="flex h-screen overflow-hidden border-collapse">
+                <Sidebar />
+                <main
+                  className="
                 min-h-screen flex-1
                 overflow-y-auto overflow-x-hidden
                 py-24 px-8
                 bg-secondary/20
                 flex flex-col
               "
-              >
-                {children}
-              </main>
-            </div>
-          </NuqsAdapter>
+                >
+                  {children}
+                </main>
+              </div>
+            </NuqsAdapter>
+          </QueryProvider>
           <Toaster />
         </ThemeProvider>
       </body>
