cf5ce7ac4509f31be1611abc161c037944a3a9c1
Mauricio <m.morocho1196@gmail.com>
2025-07-06 14:20:35 -0500

    Manage organizations

--- src/features/organization/components/organization-list.tsx
+++ src/features/organization/components/organization-list.tsx
@@ -2,23 +2,67 @@
 import { format } from "date-fns";
 import { getOrganizationsByUser } from "../query/get-organizations-by-user";
 import { redirect } from "next/navigation";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { ArrowLeftRight, ArrowUpRightFromSquare, Pen, Trash } from "lucide-react";
 
 const OrganizationList = async () => {
-  const organizationsUser = await getOrganizationsByUser();
+  const userOrganizations = await getOrganizationsByUser();
 
-  if (!organizationsUser.length) {
+  if (!userOrganizations.length) {
     redirect('/onboarding')
   }
 
   return (
-    <div>
-      {organizationsUser.map((uo) => (
-        <div key={uo.organization.id}>
-          <div>Name: {uo.organization.name}</div>
-          <div>Joined at: {format(uo.joinedAt, "yyy-MM-dd , HH:mm")}</div>
-        </div>
-      ))}
-    </div>
+    <Table>
+      <TableCaption>A list of organizations you are part of</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ID</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Joined At</TableHead>
+          <TableHead >Members</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {userOrganizations.map((uo) => {
+          const switchButton = <Button variant="outline" size="icon">
+            <ArrowLeftRight className="w-4 h-4" />
+          </Button>
+          const detailButton = <Button variant="outline" size="icon">
+            <ArrowUpRightFromSquare className="w-4 h-4" />
+          </Button>
+          const editButton = <Button variant="outline" size="icon">
+            <Pen className="w-4 h-4" />
+          </Button>
+          const deleteButton = <Button variant="destructive" size="icon">
+            <Trash className="w-4 h-4" />
+          </Button>
+
+          const buttons = (
+            <>
+              {switchButton}
+              {detailButton}
+              {editButton}
+              {deleteButton}
+            </>
+          )
+
+          return (
+            <TableRow key={uo.id}>
+              <TableCell className="font-medium">{uo.id}</TableCell>
+              <TableCell >{uo.name}</TableCell>
+              <TableCell>{format(uo.member.joinedAt, "yyy-MM-dd , HH:mm")}</TableCell>
+              <TableCell>{uo._count.members}</TableCell>
+              <TableCell className="flex justify-end gap-2">
+                {buttons}
+              </TableCell>
+
+            </TableRow>
+          )
+        })}
+      </TableBody>
+    </Table>
   )
 }
 export default OrganizationList;
--- src/features/organization/query/get-organizations-by-user.ts
+++ src/features/organization/query/get-organizations-by-user.ts
@@ -4,14 +4,36 @@ import { prisma } from "@/lib/prisma"
 export const getOrganizationsByUser = async () => {
   const userData = await requireProfile()
 
-  const userOrganizations = await prisma.userOrganization.findMany({
+  // const userOrganizations = await prisma.userOrganization.findMany({
+  //   where: {
+  //     userId: userData?.user.id
+  //   },
+  //   include: {
+  //     organization: true
+  //   }
+  // })
+
+  const userOrganizations = await prisma.organization.findMany({
     where: {
-      userId: userData?.user.id
+      members: {
+        some: {
+          userId: userData?.user.id
+        }
+      }
     },
     include: {
-      organization: true
+      members: {
+        where: {
+          userId: userData?.user.id
+        }
+      },
+      _count: {
+        select: {
+          members: true
+        }
+      }
     }
   })
 
-  return userOrganizations;
+  return userOrganizations.map(({ members, ...organization }) => ({ ...organization, member: members[0] }))
 }
