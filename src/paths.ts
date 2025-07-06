4b8c9b5ddef948494c9ece78369a9bb04f2edd12
Mauricio <m.morocho1196@gmail.com>
2025-07-06 12:44:24 -0500

    Creating more organizations

--- src/paths.ts
+++ src/paths.ts
@@ -1,10 +1,16 @@
-export const ticketsPath = "/tickets";
 export const homePath = "/";
+
+export const ticketsPath = "/tickets";
 export const ticketPath = (ticketId: string) => `/tickets/${ticketId}`
 export const ticketEditPath = (ticketId: string) => `/tickets/${ticketId}/edit`
+
 export const signUpPath = '/auth/signup'
 export const signInPath = '/auth/login'
+
 export const forgotPasswordPath = '/auth/forgot-password'
+
 export const accountProfilePath = "/account/profile";
 export const accountPasswordPath = "/account/password"
+
 export const organizationPath = "/organization"
+export const organizationCreatePath = "/organization/create"
