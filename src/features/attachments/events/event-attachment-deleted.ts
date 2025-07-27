82111726250fb8d9843fe2365f06b1e4640be045
Mauricio <m.morocho1196@gmail.com>
2025-07-26 22:54:07 -0500

    Delete polymorphic attachment

--- src/features/attachments/events/event-attachment-deleted.ts
+++ src/features/attachments/events/event-attachment-deleted.ts
@@ -2,11 +2,13 @@ import { s3 } from "@/lib/aws";
 import { inngest } from "@/lib/inngest";
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { generateS3Key } from "../utils/generate-s3-key";
+import { AttachmentEntity } from "@prisma/client";
 
 export type AttachmentDeletedEventArgs = {
   data: {
     attachmentId: string;
-    ticketId: string;
+    entityId: string;
+    entity: AttachmentEntity;
     fileName: string;
     organizationId: string;
   }
@@ -16,13 +18,13 @@ export const attachmentDeletedEvent = inngest.createFunction(
   { id: 'app/attachment.deleted' },
   { event: "app/attachment.deleted" },
   async ({ event }) => {
-    const { attachmentId, ticketId, fileName, organizationId } = event.data;
+    const { attachmentId, entityId, entity, fileName, organizationId } = event.data;
 
     try {
       await s3.send(
         new DeleteObjectCommand({
           Bucket: process.env.AWS_BUCKET_NAME,
-          Key: generateS3Key({ ticketId, organizationId, fileName, attachmentId }),
+          Key: generateS3Key({ entityId, entity, organizationId, fileName, attachmentId }),
         })
       )
     } catch (error) {
